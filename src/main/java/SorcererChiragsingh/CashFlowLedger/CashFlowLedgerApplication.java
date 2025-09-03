da22aaabfeeafdc1167512f38fa81101d91a1d3a
Chirag singh <chiragsingh8926@gmail.com>
2025-09-03 22:31:19 +0530

    spring project initialize

---
+++ src/main/java/SorcererChiragsingh/CashFlowLedger/CashFlowLedgerApplication.java
@@ -0,0 +1,13 @@
+package SorcererChiragsingh.CashFlowLedger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CashFlowLedgerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CashFlowLedgerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/SorcererChiragsingh/CashFlowLedger/CashFlowLedgerApplicationTests.java
@@ -0,0 +1,13 @@
+package SorcererChiragsingh.CashFlowLedger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CashFlowLedgerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
