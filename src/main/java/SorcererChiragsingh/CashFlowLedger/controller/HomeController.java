4f9273875529bc803c04b38f5c53a10829311ada
Chirag singh <chiragsingh8926@gmail.com>
2025-09-27 22:46:42 +0530

    Debug Java code

--- src/main/java/SorcererChiragsingh/CashFlowLedger/controller/HomeController.java
+++ src/main/java/SorcererChiragsingh/CashFlowLedger/controller/HomeController.java
@@ -1,7 +1,12 @@
 package SorcererChiragsingh.CashFlowLedger.controller;
 
+
+@RestController
+@RequestMapping({"/staus", "/health"})
+
 public class HomeController 
 {
+    @GetMapping
     public String healthCheck()  
     {
         return "Application is running fine";
