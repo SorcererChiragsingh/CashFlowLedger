ad0e3e165d783b3eac1916359c3392d0a61ed085
Chirag singh <chiragsingh8926@gmail.com>
2025-10-13 23:31:53 +0530

    Working on Profile

--- src/main/java/SorcererChiragsingh/CashFlowLedger/controller/ProfileController.java
+++ src/main/java/SorcererChiragsingh/CashFlowLedger/controller/ProfileController.java
@@ -1,5 +1,26 @@
 package SorcererChiragsingh.CashFlowLedger.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import SorcererChiragsingh.CashFlowLedger.dto.ProfileDTO;
+import SorcererChiragsingh.CashFlowLedger.service.ProfileService;
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequiredArgsConstructor
 public class ProfileController {
 
+    private final ProfileService profileService;
+//Handler Method
+
+     @PostMapping("/register")
+    public ResponseEntity<ProfileDTO> registerProfile(@RequestBody ProfileDTO profileDTO) {
+        ProfileDTO registeredProfile = profileService.registerProfile(profileDTO);
+        return ResponseEntity.status(HttpStatus.CREATED).body(registeredProfile);
+
+    }
 }
--- src/main/java/SorcererChiragsingh/CashFlowLedger/service/ProfileService.java
+++ src/main/java/SorcererChiragsingh/CashFlowLedger/service/ProfileService.java
@@ -4,11 +4,16 @@ import org.springframework.stereotype.Service;
 
 
 import SorcererChiragsingh.CashFlowLedger.repository.ProfileRepository;
+import SorcererChiragsingh.CashFlowLedger.dto.ProfileDTO;
+import SorcererChiragsingh.CashFlowLedger.entity.ProfileEntity;
+// import SorcererChiragsingh.CashFlowLedger.dto.AuthDTO;
 
+
+// import org.springframework.security.crypto.password.PasswordEncoder;
 
 import lombok.RequiredArgsConstructor;
 
-import java.util.Map;
+// import java.util.Map;
 import java.util.UUID;
 
 @Service
@@ -17,5 +22,48 @@ import java.util.UUID;
 public class ProfileService 
 {
     private final ProfileRepository profileRepository;
-    
+
+
+// Helper Method    
+    public ProfileDTO registerProfile(ProfileDTO profileDTO) 
+    {
+       ProfileEntity newProfile = toEntity(profileDTO);
+       newProfile.setActivationToken(UUID.randomUUID().toString());
+        newProfile = profileRepository.save(newProfile);
+
+
+
+       return toDTO(newProfile);
+    }
+// Helper Method
+
+    public ProfileEntity toEntity(ProfileDTO profileDTO) 
+    {
+       return ProfileEntity.builder()
+                .id(profileDTO.getId())
+                .fullName(profileDTO.getFullName())
+                .email(profileDTO.getEmail())
+                // .password(passwordEncoder.encode(profileDTO.getPassword()))
+                .profileImageUrl(profileDTO.getProfileImageUrl())
+                .createdAt(profileDTO.getCreatedAt())
+                .updatedAt(profileDTO.getUpdatedAt())
+                .build();
+
+    }
+// Helper Method
+
+   public ProfileDTO toDTO(ProfileEntity profileEntity) 
+   {
+        return ProfileDTO.builder()
+                .id(profileEntity.getId())
+                .fullName(profileEntity.getFullName())
+                .email(profileEntity.getEmail())
+                .profileImageUrl(profileEntity.getProfileImageUrl())
+                .createdAt(profileEntity.getCreatedAt())
+                .updatedAt(profileEntity.getUpdatedAt())
+                .build();
+    }
+
+
+
 }
