16b380d69e638ea62f653e8031c21ef5ab17ace3
Chirag singh <chiragsingh8926@gmail.com>
2025-11-05 00:17:43 +0530

    email smtp config

--- src/main/java/SorcererChiragsingh/CashFlowLedger/controller/ProfileController.java
+++ src/main/java/SorcererChiragsingh/CashFlowLedger/controller/ProfileController.java
@@ -2,13 +2,10 @@ package SorcererChiragsingh.CashFlowLedger.controller;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import SorcererChiragsingh.CashFlowLedger.dto.AuthDTO;
 import SorcererChiragsingh.CashFlowLedger.dto.ProfileDTO;
 import SorcererChiragsingh.CashFlowLedger.service.ProfileService;
 import lombok.RequiredArgsConstructor;
@@ -18,7 +15,8 @@ import lombok.RequiredArgsConstructor;
 public class ProfileController {
 
     private final ProfileService profileService;
-//Handler Method
+    
+//Handler Method for Registration
 
      @PostMapping("/register")
     public ResponseEntity<ProfileDTO> registerProfile(@RequestBody ProfileDTO profileDTO) {
@@ -27,45 +25,17 @@ public class ProfileController {
 
     }
 
-//Handler Method
+//Handler Method for activation
 
-    @GetMapping("/activate")
-    public ResponseEntity<String> activateProfile(@RequestParam String token) {
-        boolean isActivated = profileService.activateProfile(token);
-        if (isActivated) {
-            return ResponseEntity.ok("Profile activated successfully");
-        } else {
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Activation token not found or already used");
-        }
 
-    }
 
-//Handler Method
+//Handler Method for login
 
-    @PostMapping("/login")
-    public ResponseEntity<Map<String, Object>> login(@RequestBody AuthDTO authDTO) {
-        try {
-            if (!profileService.isAccountActive(authDTO.getEmail())) {
-                return ResponseEntity.status(HttpStatus.FORBIDDEN).body(Map.of(
-                        "message", "Account is not active. Please activate your account first."
-                ));
-            }
-            Map<String, Object> response = profileService.authenticateAndGenerateToken(authDTO);
-            return ResponseEntity.ok(response);
-        } catch (Exception e) {
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(Map.of(
-                    "message", e.getMessage()
-            ));
-        }
-    }
 
-//Handler Method
 
-    @GetMapping("/profile")
-    public ResponseEntity<ProfileDTO> getPublicProfile() {
-        ProfileDTO profileDTO = profileService.getPublicProfile(null);
-        return ResponseEntity.ok(profileDTO);
-    }
+//Handler Method for Profile
+
+
 
 
 }
